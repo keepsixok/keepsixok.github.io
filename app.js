1d3ab8efb205cba4d5f4c2237db205a1a1d855e4
KeepSix <Mitchell.Loch@gmail.com>
2022-09-17 07:37:49 -0600

    uploading projects

---
+++ app.js
@@ -0,0 +1,20 @@
+
+anime({
+    targets: '.border-bottom-nav',
+    width: ['0', '100%'],  // -> from '28px' to '100%',
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: false,
+    duration: 2500
+  });
+
+
+  
+anime({
+    targets: 'main',
+    width: ['0', '100%'],  // -> from '28px' to '100%',
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: false,
+    duration: 2500
+  });
